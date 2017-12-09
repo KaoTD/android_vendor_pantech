d5e7c431f9fb6d19cdbe1d00ee4dc956c8e82951
KaoTD <kaotd99@gmail.com>
2017-12-09 07:16:48 +0000

    fix minor bug

--- msm8974-common/Android.mk
+++ msm8974-common/Android.mk
@@ -29,6 +29,16 @@ LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := shutdownlistener
+LOCAL_MODULE_OWNER := pantech
+LOCAL_SRC_FILES := proprietary/app/shutdownlistener/shutdownlistener.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := qcnvitems
 LOCAL_MODULE_OWNER := pantech
 LOCAL_SRC_FILES := proprietary/framework/qcnvitems.jar
